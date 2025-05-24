6a76a214be997528dbef39b139ab0ebe6a412c03
luka <edulukalortk@gmail.com>
2025-05-24 19:53:47 +0400

    fetch data endpoint

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from src.reddit.fetch_subreddit_data import fetch_subreddit_data
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/fetch-subreddit-data")
+async def fetch_subreddit_data_endpoint(subreddit: str, time_filter: str = "month", limit: int = 10):
+    print(subreddit, time_filter, limit)
+    data = fetch_subreddit_data(subreddit, time_filter, limit)
+    return data
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/reddit/fetch_subreddit_data.py
+++ src/reddit/fetch_subreddit_data.py
@@ -5,10 +5,11 @@ import os
 
 load_dotenv()
 
+
 reddit = praw.Reddit(
-    client_id=os.getenv("REDDIT_CLIENT_ID"),
-    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
-    user_agent=os.getenv("REDDIT_USER_AGENT")
+    client_id=os.getenv("R_CLIENT_ID"),
+    client_secret=os.getenv("R_CLIENT_SECRET"),
+    user_agent=os.getenv("R_USER_AGENT")
 )
 
 
